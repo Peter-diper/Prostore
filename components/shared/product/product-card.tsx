dc16964bc4fe39fe677115ea7438c8c2bed3772c
peter-diper <rasoolfada2@gmail.com>
2026-08-31 04:35:36 +0330

    fit: building prodct card component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,7 +1,24 @@
 import { Product } from "../../../db/sample-data";
 import Link from "next/link";
 import Image from "next/image";
+import { Card, CardHeader } from "@/components/ui/card";
 
-const ProductCard = ({ product }: Product) => {};
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader className="p-0 items-center">
+        <link href={`/product/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            property
+          />
+        </link>
+      </CardHeader>
+    </Card>
+  );
+};
 
 export default ProductCard;
